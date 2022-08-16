7676b0d75ee48e232fd3f24271eaa1d70cecfa30
ZARIFMAC <uzbzarif@gmail.com>
2022-08-16 11:11:30 +0500

    send by file

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ def send_photo(chat_id:int, photo:str):
         answer from telegram bot
     """
     url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    data={'chat_id': chat_id, 'photo': photo}
-    answer = requests.post(url,data)
+    data={'chat_id': chat_id}
+    photo={'photo': photo}
+    answer = requests.post(url,params=data,files=photo)
     return answer.json()
 
 #Send message some text with markdown
@@ -33,5 +34,10 @@ send_message(text, chat_id)
 #Some URL photo of cat
 photo = 'https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg'
 photo_id = 'AgACAgQAAxkDAAIBNmL7McRhkP0jHAABVtCI1iKIkJc2HwAC-a0xGxGwtFDrbbOKqZjxGgEAAwIAA3MAAykE'
-dog=send_photo(chat_id, photo_id)
-pprint(dog)
+# dog=send_photo(chat_id, photo_id)
+# pprint(dog)
+
+#Open file
+with open('dog.jpg', 'rb') as photo:
+    dog=send_photo(chat_id, photo)
+    
