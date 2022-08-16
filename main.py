8a96318ce043d1b04bfdd52b18a54eb08b6a27c9
ZARIFMAC <uzbzarif@gmail.com>
2022-08-16 10:56:59 +0500

    send photo

--- main.py
+++ main.py
@@ -10,9 +10,28 @@ def send_message(text:str, chat_id:int):
     answer = requests.post(url,data)
     return answer.json()
 
-
+#Send message with photo
+def send_photo(chat_id:int, photo:str):
+    """
+    Send photo to telegram bot
+    Args:
+        chat_id: id of chat
+        photo: path to photo
+    Returns:
+        answer from telegram bot
+    """
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    data={'chat_id': chat_id, 'photo': photo}
+    answer = requests.post(url,data)
+    return answer.json()
 
 #Send message some text with markdown
 text = '*Hello* World'
 chat_id = '5575549228'
 send_message(text, chat_id)
+
+#Some URL photo of cat
+photo = 'https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg'
+
+dog=send_photo(chat_id, photo)
+print(dog)
