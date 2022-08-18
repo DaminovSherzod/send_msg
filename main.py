22a99567698bcb6f0727dc691b3eb531f9ccfbe7
ZARIFMAC <uzbzarif@gmail.com>
2022-08-18 10:37:01 +0500

    send Message test

--- main.py
+++ main.py
@@ -10,34 +10,32 @@ def send_message(text:str, chat_id:int):
     answer = requests.post(url,data)
     return answer.json()
 
-#Send message with photo
-def send_photo(chat_id:int, photo:str):
-    """
-    Send photo to telegram bot
-    Args:
-        chat_id: id of chat
-        photo: path to photo
-    Returns:
-        answer from telegram bot
-    """
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    data={'chat_id': chat_id}
-    photo={'photo': photo}
-    answer = requests.post(url,params=data,files=photo)
+#Send keyboar
+
+def send_keyboard(chat_id:int):
+    button = {
+        'text':'Button'
+    }
+
+    keyboard = [
+        [button]
+    ]
+
+    reply_markup = {
+        'keyboard':keyboard
+    }
+    data = {
+        'chat_id':chat_id,
+        'text':'TEST'
+    }
+
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    answer = requests.post(url,json=data)
+
     return answer.json()
 
-#Send message some text with markdown
-text = '*Hello* World'
+
+
+
 chat_id = '5575549228'
-send_message(text, chat_id)
-
-#Some URL photo of cat
-photo = 'https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg'
-photo_id = 'AgACAgQAAxkDAAIBNmL7McRhkP0jHAABVtCI1iKIkJc2HwAC-a0xGxGwtFDrbbOKqZjxGgEAAwIAA3MAAykE'
-# dog=send_photo(chat_id, photo_id)
-# pprint(dog)
-
-#Open file
-with open('dog.jpg', 'rb') as photo:
-    dog=send_photo(chat_id, photo)
-    
